487176a446fc94fd4f2131777a4453ab248aa560
Ved Patel <veds28112004@gmail.com>
2025-05-12 14:16:31 -0400

    Add user and API key models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
+import uuid
 
-# Create your models here.
+class ApiKey(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    key = models.UUIDField(default=uuid.uuid4, editable=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.key}'
