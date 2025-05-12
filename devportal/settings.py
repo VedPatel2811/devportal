0f2f94f8fe7c77c072ee2fb31fd35108acf5a816
Ved Patel <veds28112004@gmail.com>
2025-05-12 14:02:43 -0400

    Add basic settings and configuration

--- devportal/settings.py
+++ devportal/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +50,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
 
 ROOT_URLCONF = 'devportal.urls'
 
