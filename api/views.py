def85c1e45f9b890e52cb4ca7121992b15d7fc5a
Ved Patel <veds28112004@gmail.com>
2025-05-12 14:24:38 -0400

    Implement user authentication and API key management

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import ApiKey
+from .serializers import UserSerializer, ApiKeySerializer
+from django.contrib.auth.models import User
 
-# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ApiKeyViewSet(viewsets.ModelViewSet):
+    queryset = ApiKey.objects.all()
+    serializer_class = ApiKeySerializer
+    permission_classes = [permissions.IsAuthenticated]
